09beaf68ff8918b9d84364b09b1782e8b969b645
Austin Kellar <austinkellar@Mac-Users-MacBook-7.local>
2018-01-29 22:56:57 -0800

    separated scripts and styles into their own files and replaced jquery with angular

---
+++ Website/controllers/controller.js
@@ -0,0 +1,9 @@
+var app = angular.module('app', []);
+app.controller('mainController', ['$scope', '$window', function($scope, $window) {
+	$scope.playAlienInvasion = function() {
+		$window.open('https://themightyspidey.itch.io/alien-invasion-austin-kellar-2');
+	};
+	$scope.alienInvasionGithub = function() {
+		$window.open('https://github.com/AustinKellar/TheBoyz');
+	}
+}]);
